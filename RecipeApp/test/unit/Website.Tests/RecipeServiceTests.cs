9a4abdb5a880173ed81694e829947f04c649e546
Melissa Benua <mbenua@gmail.com>
2020-11-09 10:12:47 -0800

    Adding some changes

--- RecipeApp/test/unit/Website.Tests/RecipeServiceTests.cs
+++ RecipeApp/test/unit/Website.Tests/RecipeServiceTests.cs
@@ -1,5 +1,13 @@
 using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
 using Xunit;
+using RecipeApp.Core.ExternalModels;
+using Microsoft.Extensions.Configuration;
 
 namespace Website.Tests
 {
@@ -7,9 +15,32 @@ namespace Website.Tests
     {
         [Trait("Category", "Unit")]
         [Fact]
-        public void Test1()
+        public async Task TestCreateRecipe_Ok()
         {
-        // A comment
+            var mockConfig = new Mock<IConfiguration>();
+            mockConfig.SetupGet(s => s["RecipeConnectionKey"]).Returns("12345678901234567890123456784567");
+            mockConfig.SetupGet(s => s["JwtIssuer"]).Returns("12345");
+
+            var mockMessageHandler = new Mock<HttpMessageHandler>();
+            mockMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("")
+                });
+
+            var httpClientFactory = new Mock<IHttpClientFactory>();
+            httpClientFactory.Setup(s =>
+                s.CreateClient(It.IsAny<string>()))
+                .Returns(new HttpClient(mockMessageHandler.Object));
+
+            var recipeService = new RecipeService(httpClientFactory.Object, mockConfig.Object);
+
+            var testRecipe = new RecipeModel { UserId = "123", RecipeId = 987 };
+
+            var result = await recipeService.CreateRecipe(testRecipe);
+            Assert.True(result);
         }
     }
 }
