3403114e6a2878e943931c483fe27fcdabae4744
Alexander Buchholz <alexander.buchholz@appian.com>
2022-06-14 13:57:34 -0700

    Added a fun error on save receipt

--- RecipeApp/Website/RecipeService.cs
+++ RecipeApp/Website/RecipeService.cs
@@ -38,6 +38,7 @@ namespace Website
 
         public async Task<bool> SaveRecipe(RecipeModel recipe)
         {
+            throw new AccessViolationException("OH NO YOU CANNOT SAVE THIS");
             var client = _httpClientFactory.CreateClient("RecipeAPI");
             var jwt = Utilities.GenerateJWTToken(recipe.UserId, _JwtIssuer, _JwtKey);
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt);
